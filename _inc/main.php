4ab40d7bb517ac85ea4ed0197d37e6c2b79f4b8e
Jonathan Yombo <jonathanyombo@gmail.com>
2019-05-02 08:36:32 +0200

    [UPDATE] readapte view

--- _inc/main.php
+++ _inc/main.php
@@ -66,51 +66,49 @@
 <main>
     <div class="container">
         <div class="container-fluid">
+            <div class="pull-right">
+                <button class="btn btn-action btn-blue-grey" data-toggle="modal" data-target="#myModal">Ajouter un
+                    contact
+                </button>
+            </div>
             <table class="table table-hover">
                 <thead class="thead-inverse">
                 <tr>
-                    <th>#</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Username</th>
-                    <th>Actions</th>
+                    <th>N°</th>
+                    <th>Nom</th>
+                    <th>Postnom</th>
+                    <th>Prenom</th>
+                    <th>Numero</th>
+                    <th>Email</th>
+                    <th>Description</th>
+                    <th>genre</th>
+                    <th>Action</th>
                 </tr>
                 </thead>
                 <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Abby</td>
-                    <td>Barrett</td>
-                    <td>@abbeme</td>
-                    <td>
-                        <a class="blue-text"><i class="fa fa-user"></i></a>
-                        <a class="teal-text"><i class="fa fa-pencil"></i></a>
-                        <a class="red-text"><i class="fa fa-times"></i></a>
-                    </td>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>Danny</td>
-                    <td>Collins</td>
-                    <td>@dennis</td>
-                    <td>
-                        <a class="blue-text"><i class="fa fa-user"></i></a>
-                        <a class="teal-text"><i class="fa fa-pencil"></i></a>
-                        <a class="red-text"><i class="fa fa-times"></i></a>
-                    </td>
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td>Clara</td>
-                    <td>Ericson</td>
-                    <td>@claris</td>
-                    <td>
-                        <a class="blue-text"><i class="fa fa-user"></i></a>
-                        <a class="teal-text"><i class="fa fa-pencil"></i></a>
-                        <a class="red-text"><i class="fa fa-times"></i></a>
-                    </td>
-                </tr>
-
+                <?php
+                $contacts = getContacts();
+                $c = 1;
+                foreach ($contacts as $contact) :
+                    ?>
+                    <tr>
+                        <th scope="row"><?php echo $c ?></th>
+                        <td><?php echo strtoupper($contact['nom']) ?></td>
+                        <td><?php echo strtoupper($contact['postnom']) ?></td>
+                        <td><?php echo ucwords($contact['prenom'] )?></td>
+                        <td><?php echo $contact['numero'] ?></td>
+                        <td><?php echo $contact['email'] ?></td>
+                        <td><?php echo ucfirst($contact['genre']) ?></td>
+                        <td><?php echo ucfirst($contact['description']) ?></td>
+                        <td>
+                            <a class="teal-text"><i class="fa fa-pencil"></i></a>
+                            <a href="index.php?supprimer=<?php echo $c ?>" class="red-text"><i class="fa fa-times"></i></a>
+                        </td>
+                    </tr>
+                <?php
+                $c++;
+                endforeach;
+                ?>
                 </tbody>
             </table>
         </div>
@@ -130,7 +128,68 @@
 
 </footer>
 <!--/.Footer-->
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <form action="index.php" method="post">
+            <!--Content-->
+            <div class="modal-content">
+                <!--Header-->
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                    <h4 class="modal-title" id="myModalLabel">Ajouter un nouveau contact</h4>
+                </div>
+                <!--Body-->
+                <div class="modal-body">
+                    <div class="md-form">
+                        <input name="nom" type="text" id="form1" class="form-control" required>
+                        <label for="form1" class="">Nom</label>
+                    </div>
+                    <div class="md-form">
+                        <input name="postnom" type="text" id="form1" class="form-control" required>
+                        <label for="form1" class="">Postnom</label>
+                    </div>
+                    <div class="md-form">
+                        <input name="prenom" type="text" id="form1" class="form-control" required>
+                        <label for="form1" class="">Prenom</label>
+                    </div>
+                    <div class="md-form">
+                        <input name="numero" type="text" id="form1" class="form-control" required>
+                        <label for="form1" class="">Telephone</label>
+                    </div>
+                    <div class="md-form">
+                        <input name="email" type="email" id="form1" class="form-control" required>
+                        <label for="form1" class="">email</label>
+                    </div>
+                    <div class="md-form">
+                        <input name="description" type="text" id="form1" class="form-control" required>
+                        <label for="form1" class="">Descripton</label>
+                    </div>
+                    <div class="form-inline">
+                        <fieldset class="form-group">
+                            <input value="m" name="genre" type="radio" id="radio11" checked="checked">
+                            <label for="radio11">Masculin</label>
+                        </fieldset>
 
+                        <fieldset class="form-group">
+                            <input name="genre" type="radio" value="f" id="radio21">
+                            <label for="radio21">Feminin</label>
+                        </fieldset>
+                    </div>
+                </div>
+                <!--Footer-->
+                <div class="modal-footer">
+                    <input type="reset" class="btn btn-danger" data-dismiss="modal" value="fermer"/>
+                    <input name="ajouter" type="submit" class="btn btn-primary" value="Enregistrer"/>
+                </div>
+            </div>
+            <!--/.Content-->
+        </form>
+    </div>
+</div>
+<!-- /.Live preview-->
 <!-- SCRIPTS -->
 
 <!-- JQuery -->
@@ -141,6 +200,11 @@
 
 <!-- MDB core JavaScript -->
 <script type="text/javascript" src="assets/js/mdb.js"></script>
+<script type="text/javascript">
+    $('#myModal').on('shown.bs.modal', function () {
+        $('#myInput').focus()
+    })
+</script>
 
 </body>
 
