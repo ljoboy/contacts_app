9407dc689f35843c937730ded990c53df5f6d2cd
Jonathan Yombo <jonathanyombo@gmail.com>
2019-05-02 04:29:25 +0200

    [NEW] dbConnect func

--- _func/dbConnect.php
+++ _func/dbConnect.php
@@ -1,1 +1,15 @@
 <?php
+/**
+ * Connexion a la base des donnees en retournant un objet PDO
+ *
+ * @return PDO
+ */
+function dbConnect(){
+    try{
+        $db = new PDO('mysql:dbname=contacts_db;host=localhost', 'root', '');
+    }catch (PDOException $e){
+        die('Erreur due à : '.$e->getMessage());
+    }
+
+    return $db;
+}
