8875fcd6bf8b6cfe208c1029c114b0c66aa141d2
Jonathan Yombo <jonathanyombo@gmail.com>
2019-05-02 05:02:47 +0200

    [UPDATE] func to add contact

--- _func/functions.php
+++ _func/functions.php
@@ -12,4 +12,27 @@
         return $count === count($contact);
     }
 
-
+    function addContact($contact){
+        if (is_contact($contact)){
+            $verif = true;
+            try{
+                $db = dbConnect();
+                $db->prepare("INSERT INTO contacts(idContact, nom, postnom, prenom, numero, email, description, cree_le, etat, genre)
+                                        VALUES(null, :nom, :postnom, :prenom, :numero, :email, :description, now(), :etat, :genre)");
+                $db->execute([
+                    "nom" => $contact['nom'],
+                    "postnom" => $contact['postnom'],
+                    "prenom" => $contact['prenom'],
+                    "numero" => $contact['numero'],
+                    "email" => $contact['email'],
+                    "description" => $contact['description'],
+                    "etat" => $contact['etat'],
+                    "genre" => $contact['genre']
+                ]);
+            }catch (PDOException $e){
+                $verif = false;
+            }finally{
+                return $verif;
+            }
+        }
+    }
